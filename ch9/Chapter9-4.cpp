79de8ddf8777efb1b139385ba7459c8751b005fa
hrin209 <164707937+HyerinYoon012@users.noreply.github.com>
2025-04-13 12:43:17 +0900

    Update Chapter9-4.cpp

--- ch9/Chapter9-4.cpp
+++ ch9/Chapter9-4.cpp
@@ -1,59 +1,75 @@
 #include<iostream>
-#include<string>
 using namespace std;
 
 class LoopAdder {
+	string name;
+	int x, y, sum;
+	void read();
+	void write();
+protected:
+	LoopAdder(string name = "") {
+		this->name = name;
+	}
+	int getX() { return x; }
+	int getY() { return y; }
+	virtual int calculate() = 0;
 public:
-	virtual void run() = 0;  // 순수 가상 함수
+	void run();
 };
+
+void LoopAdder::read() {
+	cout << name << ":" << endl;
+	cout << "처음 수에서 두번째 수까지 더합니다. 두 수를 입력하세요 >> ";
+	cin >> x >> y;
+}
+
+void LoopAdder::write() {
+	cout << x << "에서" << y << "까지의 합 = " << sum << " 입니다" << endl;
+}
+
+void LoopAdder::run() {
+	read();
+	sum = calculate();
+	write();
+}
+
 
 class WhileLoopAdder : public LoopAdder {
-	int a, b;
+	int x, y;
 	int sum = 0;
-	int i = 0;
-	string s;
 public:
-	WhileLoopAdder(const string& s) {
-		this->s = s;
-	}
-	virtual void run() {
-		cout << s << ":" << endl << "처음 수에서 두번째 수까지 더합니다. 두 수를 입력하세요 >> ";
-		cin >> a >> b;
-		int i = a;
-		while (i <= b) {
-			sum += i;
-			i++;
+	WhileLoopAdder(const string& s) : LoopAdder(s) {}
+	virtual int calculate() {
+		x = getX();
+		y = getY();
+		while (x <= y) {
+			sum += x;
+			x++;
 		}
-		cout << a << "에서 " << b << "까지의 합 = " << sum << "입니다" << endl;
+		return sum;
 	}
 };
 
-class DoWhileLoopAdder : public LoopAdder {
-	int a, b;
-	int sum = 0;
-	int i = 0;
-	string s;
-public:
-	DoWhileLoopAdder(const string& s) {
-		this->s = s;
+	class DoWhileLoopAdder : public LoopAdder {
+		int x, y;
+		int sum = 0;
+	public:
+		DoWhileLoopAdder(const string& s) : LoopAdder(s) {}
+		virtual int calculate() {
+			x = getX();
+			y = getY();
+			do {
+				sum += x;
+				x++;
+			} while (x <= y);
+			return sum;
+		}
+	};
 
+	int main() {
+		WhileLoopAdder whileLoop("While Loop");
+		DoWhileLoopAdder doWhileLoop("Do while Loop");
+
+		whileLoop.run();
+		doWhileLoop.run();
 	}
-	virtual void run() {
-		cout << s << ":" << endl << "처음 수에서 두번째 수까지 더합니다. 두 수를 입력하세요 >> ";
-		cin >> a >> b;
-		int i = a;
-		do {
-			sum += i;
-			i++;
-		} while (i <= b);
-		cout << a << "에서 " << b << "까지의 합 = " << sum << "입니다" << endl;
-	}
-};
-
-int main() {
-	WhileLoopAdder whileLoop("While Loop");
-	DoWhileLoopAdder doWhileLoop("Do while Loop");
-
-	whileLoop.run();
-	doWhileLoop.run();
-}
